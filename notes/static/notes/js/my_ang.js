55b9fe375f9df806f60829ea7049b895091fef44
jeezybrick <smooker14@gmail.com>
2015-09-06 06:24:26 +0300

    add angular app and controller login

---
+++ notes/static/notes/js/my_ang.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Jeezy on 06.09.2015.
+ */
+
+
+var myApp = angular.module('myApp', []),
+
+    url = '/cart/add/';
+
+//Контроллер для логин-формы
+myApp.controller('loginCtrl', function ($scope, $http) {
+    //
+});
