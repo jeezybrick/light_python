cd7960db1704d5b46b3ee798f99f069452039e7d
Andrey Statsenko <smooker14@gmail.com>
2015-09-08 05:55:42 +0300

    Исправлены настройки

--- Light/settings.py
+++ Light/settings.py
@@ -81,7 +81,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    },
+    'alternative': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_light',
+        'USER': 'Ваш логин',
+        'PASSWORD': 'Ваш пароль',
+        'HOST': '127.0.0.1',
+    },
 }
 
 
